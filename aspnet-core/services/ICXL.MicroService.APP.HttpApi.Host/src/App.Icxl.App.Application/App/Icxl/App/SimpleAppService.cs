6f5a01b5151ee926927fcc4f207bf02f56cf126d
icxldd <1278245922@qq.com>
2024-08-23 16:59:24 +0800

    update

--- aspnet-core/services/ICXL.MicroService.APP.HttpApi.Host/src/App.Icxl.App.Application/App/Icxl/App/SimpleAppService.cs
+++ aspnet-core/services/ICXL.MicroService.APP.HttpApi.Host/src/App.Icxl.App.Application/App/Icxl/App/SimpleAppService.cs
@@ -1,12 +1,30 @@
 ﻿using System.Threading.Tasks;
+using Volo.Abp.Users;
 
 namespace App.Icxl.App;
 
 
-public class SimpleAppService:AppAppServiceBase, ISimpleAppService
+public class SimpleAppService : AppAppServiceBase, ISimpleAppService
 {
+
+    public ICurrentUser currentUser { get; set; }
+
+    public SimpleAppService(ICurrentUser currentUser)
+    {
+        this.currentUser = currentUser;
+    }
+
     public Task<string> TestApi(string text)
     {
-        return Task.FromResult("123");
+
+        if (currentUser.Id.HasValue)
+        {
+            return Task.FromResult(currentUser.Id.ToString());
+        }
+        else
+        {
+
+            return Task.FromResult("123");
+        }
     }
 }
